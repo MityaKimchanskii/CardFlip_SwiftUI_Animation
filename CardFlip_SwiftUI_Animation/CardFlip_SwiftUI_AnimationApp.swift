4199bba0a3b2693215c3d3264208198b58703b50
Dmitrii Kim <mitya_kim@icloud.com>
2023-03-30 15:53:57 -0700

    Initial Commit

---
+++ CardFlip_SwiftUI_Animation/CardFlip_SwiftUI_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardFlip_SwiftUI_AnimationApp.swift
+//  CardFlip_SwiftUI_Animation
+//
+//  Created by Mitya Kim on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct CardFlip_SwiftUI_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
